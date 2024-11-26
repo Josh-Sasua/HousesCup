4a27175f0f2571630dc5bcfad38a5f950df046f9
Joshua Castellani <joshuacastellani@gmail.com>
2024-11-26 15:21:21 +0100

    :)

---
+++ src/test/java/org/generation/italy/ravenclaw/HousesCup/HousesCupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.italy.ravenclaw.HousesCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousesCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
